a0686808f7ce2b4dc999198373cf77fdfdbeca8f
Luke Adams <luke4450@gmail.com>
2015-04-04 23:10:53 -0500

    Now exports to folder properly ~

--- autoFBX.py
+++ autoFBX.py
@@ -7,11 +7,13 @@ bl_info = {"name": "autoFBX", "category": "Import-Export"}
 @persistent
 def save_fbx(scene):
 	#Edit filename to end in .fbx
-	newpath = os.path.splitext(bpy.data.filepath)[0] + '-groups'
+	newpath = os.path.splitext(bpy.data.filepath)[0] + '-groups/'
+
+	if not os.path.exists(newpath):
+		os.makedirs(newpath)
 
 	print('AutoFBX::Exporting to FBX in current directory...')
-	bpy.ops.export_scene.fbx(check_existing=False, filepath=newpath,  
-		version='BIN7400', mesh_smooth_type='FACE', batch_mode='GROUP', use_batch_own_dir=False, use_selection=False, axis_forward='X', axis_up='-Z', global_scale=100)
+	bpy.ops.export_scene.fbx(check_existing=False, filepath=newpath, version='BIN7400', mesh_smooth_type='FACE', batch_mode='GROUP', use_batch_own_dir=False, use_selection=False, axis_forward='X', axis_up='-Z', global_scale=100)
 
 
 def register():
